ef3c73890c307586870b7831b16a097e033d40f2
Andrey Novikov <andrenovikov2000@mail.ru>
2020-08-02 12:56:12 +0600

    Initial Commit

---
+++ IMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IMusic
+//
+//  Created by Andrey Novikov on 8/2/20.
+//  Copyright © 2020 Andrey Novikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
